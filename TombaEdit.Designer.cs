83d0578682049fc5bb8699f5fee3df8b8ce05100
Lovre Mrčela <lovre.mrcela@fer.hr>
2019-07-29 14:56:00 +0200

    Empty application

---
+++ TombaEdit.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace TombaEdit
+{
+    partial class TombaEdit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public string Version
+        {
+            get { return "v. 0.1"; }
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TombaEdit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "TombaEdit";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
