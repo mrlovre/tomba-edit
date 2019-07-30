b32b5f7d718a7a54594859826cc8f573bad2af44
Lovre Mrčela <lovre.mrcela@fer.hr>
2019-07-30 15:53:28 +0200

    Integrate GAM pack into GUI.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,31 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
+using TombaEdit.Logic;
 
 namespace TombaEdit
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        public static string Version => "TombaEdit - v. 0.1";
-
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
+        }
+
+        public static string Version => "TombaEdit - v. 0.1";
+
+
+        private void ButtonInputFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            if ((bool) dialog.ShowDialog(this)) TextInputFile.Text = dialog.FileName;
+        }
+
+        private void ButtonOutputFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            if ((bool) dialog.ShowDialog(this)) TextOutputFile.Text = dialog.FileName;
+        }
+
+        private void ButtonPack_OnClick(object sender, RoutedEventArgs e)
+        {
+            Gam.Pack(TextInputFile.Text, TextOutputFile.Text);
+        }
+
+        private void ClearInputFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            TextInputFile.Text = "";
+        }
+
+        private void ClearOutputFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            TextOutputFile.Text = "";
+        }
+
+        private void CopyOutputFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            DoCopyOutputFile();
+        }
+
+        private void DoCopyOutputFile()
+        {
+            var outFilename = Path.ChangeExtension(TextInputFile.Text, "gam");
+            TextOutputFile.Text = outFilename;
+        }
+
+        private void RefreshButtonPack()
+        {
+            ButtonPack.IsEnabled = TextInputFile.Text.Length != 0 && TextOutputFile.Text.Length != 0;
+        }
+
+        private void TextInputFile_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (TextInputFile.Text.Length != 0 && TextOutputFile.Text.Length == 0) DoCopyOutputFile();
+            ClearInputFile.IsEnabled = TextInputFile.Text.Length != 0;
+            CopyOutputFile.IsEnabled = TextInputFile.Text.Length != 0;
+            RefreshButtonPack();
+        }
+
+        private void TextOutputFile_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            ClearOutputFile.IsEnabled = TextOutputFile.Text.Length != 0;
+            RefreshButtonPack();
         }
     }
 }
